1eb83b176738cc78ce84f45d3d71ed28da36ebff
Peter Kazarinoff <peter.kazarinoff@gmail.com>
2018-07-31 12:09:33 -0700

    adds

--- datatools.py
+++ datatools.py
@@ -17,3 +17,19 @@ class DataPoint:
     
     def __repr__(self):
         return f"Data Point: {self.data} Date: {self.date_time} Field: {self.field} Channel: {self.channel}"
+
+def generate_API_key(N=8):
+    """
+    generates a random string N characters long
+    made up of uppercase ASCII characters (letters and numbers)
+
+    example:
+
+        >>> generate_API_key(8)
+
+        'AP9UB73A'
+
+    :param N: int, length of random API key
+    :return: str, random string of uppercase ASCII characters ex: 'NP9TRG54'
+    """
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
--- myproject.py
+++ myproject.py
@@ -5,6 +5,8 @@ import os
 import sqlite3
 from dbtools import createdb
 import dateutil.parser
+from config import API_KEY, MAC_ADDRESS
+import pytz
 
 app = Flask(__name__)
 
@@ -15,39 +17,63 @@ createdb('data.db')
 def index():
     r = requests.get('https://api.thingspeak.com/channels/254616/fields/1/last.txt')
     temp_c = r.text
-    temp = str(round(((9.0 / 5.0 ) * float(temp_c) + 32),1) ) + ' F'
-    #temp = '90'
+    temp = str(round(((9.0 / 5.0) * float(temp_c) + 32), 1)) + ' F'
+    # temp = '90'
     r_in = requests.get('https://api.thingspeak.com/channels/254616/fields/2/last.txt')
     temp_c_in = r_in.text
-    temp_in = str(round(((9.0 / 5.0) * float(temp_c_in) + 32),1) ) + ' F'
+    temp_in = str(round(((9.0 / 5.0) * float(temp_c_in) + 32), 1)) + ' F'
     return render_template("index.html", temp_out=temp, temp_in=temp_in)
 
+
 @app.route("/showrecent")
-def showrecent():
+def show_recent():
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
     c.execute('SELECT * FROM data WHERE ID = (SELECT MAX(ID) FROM data);')
     row = c.fetchone()
+    c.close()
     conn.close()
-    data = str(round(row[5],1))
+    data = str(round(row[5], 1))
     time_str = row[2]
     t = dateutil.parser.parse(time_str)
-    time_stamp = t.strftime('%I:%M %p +%S sec %b %d, %Y')
+    t_pst = t.astimezone(pytz.timezone('US/Pacific'))
+    time_stamp = t_pst.strftime('%I:%M:%S %p Time Zone: %Z   %b %d, %Y')
     return render_template("showrecent.html", data=data, time_stamp=time_stamp)
 
+@app.route("/showtemp")
+def show_temp():
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM data WHERE ID = (SELECT MAX(ID) FROM data);')
+    row = c.fetchone()
+    c.close()
+    conn.close()
+    data = str(round((float(row[5]) * 1.8) + 32))
+    time_str = row[2]
+    t = dateutil.parser.parse(time_str)
+    t_pst = t.astimezone(pytz.timezone('US/Pacific'))
+    time_stamp = t_pst.strftime('%I:%M:%S %p   %b %d, %Y')
+    return render_template("showlocaltemp.html", data=data, time_stamp=time_stamp)
 
 @app.route("/update/API_key=<api_key>/mac=<mac>/field=<int:field>/data=<data>", methods=['GET'])
 def write_data_point(api_key, mac, field, data):
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    t = datetime.datetime.now()
-    date_time_str = t.isoformat()
-    c.execute("INSERT INTO data VALUES(:Id, :API_key, :date_time, :mac, :field, :data)",
-              {'Id':None,'API_key': api_key, 'date_time': date_time_str, 'mac': mac, 'field': int(field), 'data': round(float(data),4)})
-    conn.commit()
-    conn.close()
+    if (api_key == API_KEY and mac == MAC_ADDRESS):
+        conn = sqlite3.connect('data.db')
+        c = conn.cursor()
+        t = datetime.datetime.now(tz=pytz.utc)
+        date_time_str = t.isoformat()
+        c.execute("INSERT INTO data VALUES(:Id, :API_key, :date_time, :mac, :field, :data)",
+              {'Id': None, 'API_key': api_key, 'date_time': date_time_str, 'mac': mac, 'field': int(field),
+               'data': round(float(data), 4)})
+        conn.commit()
+        c.close()
+        conn.close()
 
-    return render_template("showrecent.html", data = data, time_stamp=date_time_str)
+        return render_template("showrecent.html", data=data, time_stamp=date_time_str)
+
+    else:
+        return render_template("403.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
